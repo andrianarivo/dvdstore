7f1edecc5e6f91e0fd1ae93be48fa6f27500cd6f
david <andrianarivo.david@gmail.com>
2024-01-22 19:45:21 +0300

    Solution 3

--- core/src/main/java/com/mycompany/dvdstore/repository/MovieRepository.java
+++ core/src/main/java/com/mycompany/dvdstore/repository/MovieRepository.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 import com.mycompany.dvdstore.entity.Movie;
 
-public class MovieRepository {
+public class MovieRepository implements MovieRepositoryInterface {
   private List<Movie> movies = new ArrayList<>();
+
   public void add(Movie movie) {
     movies.add(movie);
     System.out.println("The movie " + movie.getTitle() + " has been added");
--- core/src/main/java/com/mycompany/dvdstore/service/MovieService.java
+++ core/src/main/java/com/mycompany/dvdstore/service/MovieService.java
@@ -3,7 +3,7 @@ package com.mycompany.dvdstore.service;
 import com.mycompany.dvdstore.entity.Movie;
 import com.mycompany.dvdstore.repository.GoLiveMovieRepository;
 
-public class MovieService {
+public class MovieService implements MovieServiceInterface {
   private GoLiveMovieRepository movieRepository = new GoLiveMovieRepository();
 
   public void registerMovie(Movie movie) {
