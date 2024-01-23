9238804ea34abfa25802cd3f7d2448eb1f774599
david <andrianarivo.david@gmail.com>
2024-01-23 17:18:50 +0300

    Solution 5

--- core/src/main/java/com/mycompany/dvdstore/App.java
+++ core/src/main/java/com/mycompany/dvdstore/App.java
@@ -1,8 +1,8 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
-import com.mycompany.dvdstore.repository.GoLiveMovieRepository;
-import com.mycompany.dvdstore.service.MovieService;
+import com.mycompany.dvdstore.repository.FileMovieRepository;
+import com.mycompany.dvdstore.service.DefaultMovieService;
 
 /**
  * Hello world!
@@ -11,8 +11,8 @@ import com.mycompany.dvdstore.service.MovieService;
 public class App {
   public static void main(String[] args) {
     MovieController movieController = new MovieController();
-    MovieService movieService = new MovieService();
-    GoLiveMovieRepository movieRepository = new GoLiveMovieRepository();
+    DefaultMovieService movieService = new DefaultMovieService();
+    FileMovieRepository movieRepository = new FileMovieRepository();
     movieService.setMovieRepository(movieRepository);
     movieController.setMovieService(movieService);
     movieController.addUsingConsole();
--- core/src/main/java/com/mycompany/dvdstore/repository/MemoryMovieRepository.java
+++ core/src/main/java/com/mycompany/dvdstore/repository/MemoryMovieRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import com.mycompany.dvdstore.entity.Movie;
 
-public class MovieRepository implements MovieRepositoryInterface {
+public class MemoryMovieRepository implements MovieRepositoryInterface {
   private List<Movie> movies = new ArrayList<>();
 
   public void add(Movie movie) {
