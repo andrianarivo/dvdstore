1ee2dfcdb709b165da0babf795b522747e9bc024
david <andrianarivo.david@gmail.com>
2024-01-23 19:06:50 +0300

    Solution 6

--- core/src/main/java/com/mycompany/dvdstore/App.java
+++ core/src/main/java/com/mycompany/dvdstore/App.java
@@ -1,8 +1,8 @@
 package com.mycompany.dvdstore;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.mycompany.dvdstore.controller.MovieController;
-import com.mycompany.dvdstore.repository.FileMovieRepository;
-import com.mycompany.dvdstore.service.DefaultMovieService;
 
 /**
  * Hello world!
@@ -10,11 +10,8 @@ import com.mycompany.dvdstore.service.DefaultMovieService;
  */
 public class App {
   public static void main(String[] args) {
-    MovieController movieController = new MovieController();
-    DefaultMovieService movieService = new DefaultMovieService();
-    FileMovieRepository movieRepository = new FileMovieRepository();
-    movieService.setMovieRepository(movieRepository);
-    movieController.setMovieService(movieService);
-    movieController.addUsingConsole();
+    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    MovieController controller = context.getBean(MovieController.class);
+    controller.addUsingConsole();
   }
 }
