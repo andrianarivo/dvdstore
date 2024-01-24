fb8e7757a66869dd97d33e1281cad53c43036446
david <andrianarivo.david@gmail.com>
2024-01-24 19:01:01 +0300

    Solution 7

--- core/src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
+++ core/src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
@@ -1,15 +1,26 @@
 package com.mycompany.dvdstore.repository;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 import com.mycompany.dvdstore.entity.Movie;
 
 public class FileMovieRepository implements MovieRepositoryInterface {
+  private File file;
+
+  public File getFile() {
+    return file;
+  }
+
+  public void setFile(File file) {
+    this.file = file;
+  }
+
   public void add(Movie movie) {
     FileWriter writer;
     try {
-      writer = new FileWriter("movies.txt", true);
+      writer = new FileWriter(file, true);
       writer.write(movie.getTitle() + ";" + movie.getGenre() + "\n");
       writer.close();
     } catch (IOException e) {
