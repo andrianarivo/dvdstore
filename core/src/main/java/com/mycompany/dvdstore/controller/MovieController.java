449928420f6a379060f918da3c471649c895fade
david <andrianarivo.david@gmail.com>
2024-01-29 18:44:02 +0300

    Solution 16

--- core/src/main/java/com/mycompany/dvdstore/controller/MovieController.java
+++ core/src/main/java/com/mycompany/dvdstore/controller/MovieController.java
@@ -15,14 +15,15 @@ public class MovieController {
 
   public void addUsingConsole() {
     System.out.println("Enter movie title: ");
-    Scanner sc = new Scanner(System.in);
-    String title = sc.nextLine();
-    System.out.println("Enter movie genre: ");
-    String genre = sc.nextLine();
-    Movie movie = new Movie();
-    movie.setTitle(title);
-    movie.setGenre(genre);
-    movieService.registerMovie(movie);
+    try (Scanner sc = new Scanner(System.in)) {
+      String title = sc.nextLine();
+      System.out.println("Enter movie genre: ");
+      String genre = sc.nextLine();
+      Movie movie = new Movie();
+      movie.setTitle(title);
+      movie.setGenre(genre);
+      movieService.registerMovie(movie);
+    }
   }
 
   public MovieServiceInterface getMovieService() {
--- core/src/main/java/com/mycompany/dvdstore/repository/memory/MemoryMovieRepository.java
+++ core/src/main/java/com/mycompany/dvdstore/repository/memory/MemoryMovieRepository.java
@@ -2,7 +2,6 @@ package com.mycompany.dvdstore.repository.memory;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.stereotype.Repository;
 import com.mycompany.dvdstore.entity.Movie;
 import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
 
