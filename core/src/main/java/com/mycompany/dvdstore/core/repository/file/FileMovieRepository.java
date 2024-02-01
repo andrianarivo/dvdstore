499b751ff28d958839804540c25bb3234cb19ed0
David Andrianarivo <andrianarivo.david@gmail.com>
2024-02-01 15:44:34 +0300

    Solution 24

--- core/src/main/java/com/mycompany/dvdstore/core/repository/file/FileMovieRepository.java
+++ core/src/main/java/com/mycompany/dvdstore/core/repository/file/FileMovieRepository.java
@@ -27,10 +27,13 @@ public class FileMovieRepository implements MovieRepositoryInterface {
   }
 
   public void add(Movie movie) {
+    long lastId = list().stream().map(Movie::getId).max(Long::compare).orElse(0L);
+    movie.setId(lastId + 1);
     FileWriter writer;
     try {
       writer = new FileWriter(file, true);
-      writer.write(movie.getTitle() + ";" + movie.getGenre() + "\n");
+      writer.write(movie.getId() + ";" + movie.getTitle() + ";" + movie.getGenre() + ";"
+          + movie.getDescription() + "\n");
       writer.close();
     } catch (IOException e) {
       e.printStackTrace();
--- web/src/main/java/com/mycompany/dvdstore/web/controller/MovieController.java
+++ web/src/main/java/com/mycompany/dvdstore/web/controller/MovieController.java
@@ -1,12 +1,12 @@
 package com.mycompany.dvdstore.web.controller;
 
-import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 import com.mycompany.dvdstore.core.entity.Movie;
 import com.mycompany.dvdstore.core.service.MovieServiceInterface;
 
@@ -23,21 +23,18 @@ public class MovieController {
     return movieService;
   }
 
-  @RequestMapping("/home")
-  public ModelAndView displayHome() {
-    System.out.println("displayHome() called");
-    List<Movie> movies = movieService.getMovies();
-    ModelAndView modelAndView = new ModelAndView("dvdstore-home");
-    modelAndView.addObject("movies", movies);
-    return modelAndView;
-  }
-
-  @RequestMapping("/{id}")
+  @GetMapping("/{id}")
   public String displayMovieCard(@PathVariable("id") Long id, Model model) {
     System.out.println("displayMovieCard() called");
     Movie movie = movieService.getMovieById(id);
     model.addAttribute("movie", movie);
     return "dvdstore-details";
+  }
+
+  @PostMapping("")
+  public String addMovie(@ModelAttribute Movie movie) {
+    movieService.registerMovie(movie);
+    return "movie-added";
   }
 
 }
